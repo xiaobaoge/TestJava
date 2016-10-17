d752a5064af2503749bf9e090609a096320a314b
U-YH-20160822JGDC\Administrator <shizifengche@163.com>
2016-10-17 13:01:26 +0800

    second

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TestJava
 about personal test java code
 a lit modify
+#this is a test
---
+++ TestJava.java
@@ -0,0 +1,9 @@
+public TestJava{
+	public TestJava(){
+	
+	}
+	public static void main(String args[]){
+	
+	
+	}
+}
